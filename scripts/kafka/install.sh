0ab587b9dfdc7481465dbecf9f62e3dda2aef9b8
Dmytro Brazhnyk <amid.ukr@gmail.com>
2020-04-13 01:27:18 -0400

    Scripts to install kafka in Kubernetes

---
+++ scripts/kafka/install.sh
@@ -0,0 +1,5 @@
+KD_SCRIPT_VARS_FILE=$(dirname $0)/../script-vars.sh
+source $KD_SCRIPT_VARS_FILE
+
+kubectl apply -f $KD_SCRIPT_DIRECTORY/kafka/k8s-yaml/zookeeper.yaml
+kubectl apply -f $KD_SCRIPT_DIRECTORY/kafka/k8s-yaml/kafka.yaml
---
+++ scripts/kafka/k8s-yaml/README.md
@@ -0,0 +1,2 @@
+Templates based on instruction: https://dzone.com/articles/ultimate-guide-to-installing-kafka-docker-on-kuber
+
